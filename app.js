7319e836288655b5853198836e6eb8ab6fede1bf
SquadraCorse <info@maas38.nl>
2014-08-22 00:12:00 +0200

    new version

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 (function (ng) {
     'use strict';
-    ng.module('app', ['afklm.ng.lazyImage'])
+    ng.module('app', ['afkl.ng.lazyImage'])
         .controller('mainCtrl', ['$scope', function mainCtrl($scope) {
             // TODO generate url overhere for demo...
 
